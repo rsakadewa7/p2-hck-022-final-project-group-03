8418c15b3825ef63c1733d2749b7ba611e7ea5de
unknown <fdzakimkerja@gmail.com>
2024-12-18 08:58:13 +0700

    EDA

---
+++ eda.py
@@ -0,0 +1,16 @@
+# import library
+import pandas as pd 
+import streamlit as st 
+
+# library for visualization
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
+def run():
+    # set title
+    st.title('Bank Marketing Campaign Analysis')
+
+
+
+if __name__ == '__main__':
+    run()
